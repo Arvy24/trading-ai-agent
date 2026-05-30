d481bb5f029acdb1fee04da5393d90482c86c061
Arvy24 <admin@arvy24.co.uk>
2026-05-30 23:07:33 +0100

    Remove root package-lock to fix Vercel build

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,13 +1,8 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
+export default {
   content: [
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./public/**/*.html"
   ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
+}
